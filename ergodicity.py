17c7cbee9331a188552c1d588d86cd59ebbb1227
lsswchmnn <lasse.wiechmann@web.de>
2025-12-23 14:15:34 +0100

    Datei für Ergodizitäts-Untersuchungen ergänzt

---
+++ ergodicity.py
@@ -0,0 +1,11 @@
+import numpy as np
+from typing import List
+#=========================================================================
+def calculate_ergodicity(paths: List[List[float]]):
+    data = np.array(paths)
+
+def time_average(path):
+    pass
+
+def ensemble_average(paths, t):
+    pass
