0f654082a1f662c73e0d672c848e82b9accdca62
lsswchmnn <lasse.wiechmann@web.de>
2026-06-27 11:37:11 +0200

    Startmenü ergänzt, an neues Controller-Modell angepasst

--- Implementierung/main.py
+++ Implementierung/main.py
@@ -1,7 +1,19 @@
-from core.simulation import MonteCarloSim
+from core.controller import Controller
 from ui.cli          import CLI
 #=========================================================================
 if __name__ == "__main__":
-    sim = MonteCarloSim()
-    cli = CLI(sim)
-    cli.run()
+    
+    controller = Controller()
+    
+    while True:
+        print("MONTE-CARLO-SIMULATION\n")
+        print("1 - Start CLI")
+        print("C - Cancel")
+        # evtl. später GUI oder Web
+        choice = input("> ").strip().lower()
+    
+        if choice == "1":
+            cli = CLI(controller)
+            cli.run()
+        elif choice == "c":
+            break
