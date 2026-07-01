bc940b84427e6260e5dc2e467f24a1e049c8ce8f
lsswchmnn <lasse.wiechmann@web.de>
2026-07-01 08:30:11 +0200

    Parallele Startstates für ND eingefügt

--- Implementierung/process/start_states.py
+++ Implementierung/process/start_states.py
@@ -1,6 +1,18 @@
+import numpy as np
 #=========================================================================
+# 1D-Startstates
+
 def fixed_state(rng) -> float:
     return 1
 
 def random_state(rng) -> float:
     return rng.gauss(0, 1)
+
+#=========================================================================
+# ND-Startstates
+
+def fixed_state_nd(rng, n_dimensions: int = 2) -> np.ndarray:
+    return np.zeros(n_dimensions)
+
+def random_state_nd(rng, n_dimensions: int = 2) -> np.ndarray:
+    return np.array([rng.gauss(0,1) for _ in range(n_dimensions)])
