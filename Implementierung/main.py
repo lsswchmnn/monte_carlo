f5bed855e20f9c01f0a1d72194cb85a5dbf36ac2
lsswchmnn <lasse.wiechmann@web.de>
2026-07-26 09:26:47 +0200

    main überarbeitet: Menü mit Auswahl zwischen CLI und Streamlit

--- Implementierung/main.py
+++ Implementierung/main.py
@@ -1,22 +1,35 @@
-from core.controller import Controller
-from ui.cli.cli      import CLI
+import subprocess
+import sys
+from   core.controller  import Controller
+from   ui.cli.cli       import CLI
 #=========================================================================
-if __name__ == "__main__":
-    
-    controller = Controller()
-    
-    while True:
-        # Sobald GUI oder Web implementiert, hier Startmenü
 
-        # print("1 - Start CLI")
-        # print("C - Cancel")
-        # choice = input("> ").strip().lower()
-    
-        choice = "1"
+if __name__ == "__main__":
+
+    controller = Controller()
+
+    while True:
+        print("1 - Start CLI")
+        print("2 - Start Streamlit")
+        print("C - Cancel")
+        choice = input("> ").strip().lower()
 
         if choice == "1":
             cli = CLI(controller)
             cli.run()
             break
+
+        elif choice == "2":
+            subprocess.run(
+                [
+                    sys.executable,
+                    "-m",
+                    "streamlit",
+                    "run",
+                    "ui/web/app.py"
+                ]
+            )
+            break
+
         elif choice == "c":
             break
